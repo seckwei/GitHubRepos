8876f7ab04d5b1846b7a9efa88e5286a91268aba
SeckWei <seckwei@users.noreply.github.com>
2017-01-06 15:14:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const GitHubAPI = {
-    fetchUserRepo (username, props) {
-        return new Promise((resolve, reject) => {
-            
-            var oReq = new XMLHttpRequest(),
-                self = this;
-            
-            oReq.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    resolve(self.filterRepoProp(props, oReq.responseText));
-                }
-            };
-            
-            oReq.open('GET', 'https://api.github.com/search/repositories?q=user:' + username);
-            oReq.send(); 
-        });
-    },
-    
-    filterRepoProp (props, response) {
-        var res = JSON.parse(response);
-        return res.items.map(repo => {
-            var obj = {};
-            props.forEach(prop => {
-               obj[prop] = repo[prop] !== null ? repo[prop] : ''; 
-            });
-            return obj;
-        });
-    }
-}
+((global) => {
+    const GitHubAPI = {
+        fetchUserRepo (username, props) {
+            return new Promise((resolve, reject) => {
 
-module.exports = GitHubAPI;
+                var oReq = new XMLHttpRequest(),
+                    self = this;
+
+                oReq.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        resolve(self.filterRepoProp(props, oReq.responseText));
+                    }
+                };
+
+                oReq.open('GET', 'https://api.github.com/search/repositories?q=user:' + username);
+                oReq.send(); 
+            });
+        },
+
+        filterRepoProp (props, response) {
+            var res = JSON.parse(response);
+            return res.items.map(repo => {
+                var obj = {};
+                props.forEach(prop => {
+                   obj[prop] = repo[prop] !== null ? repo[prop] : ''; 
+                });
+                return obj;
+            });
+        }
+    }
+    
+    if(module) {
+        module.exports = GitHubAPI;
+    }
+    else {
+        global.GitHubAPI = GitHubAPI;
+    }
+})(window || global);
